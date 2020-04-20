920d3be8eb72900eb1e888460545ad0618cf1715
Pris Craftsmn <prisci41@gmail.com>
2020-04-20 15:50:05 -0400

    Refactored

---
+++ src/navbar/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>
+                <ul>
+                    <li><a href="#home"> Home </a></li>
+                    <li><a href="#programs"> Programs </a></li>
+                    <li><a href="#Bootcamps"> Boot Camps </a></li>
+                    <li><a href="#contact us"> Contact </a></li>
+                </ul>
+            </h1>
+        </div>
+    );
+}
+
+export default App;
